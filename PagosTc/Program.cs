51691c0968c6553563ab9b4c4cfd409391c7b9a2
kamiarnold73 <kamilla_arnold24@unikino.edu.mx>
2024-10-02 07:57:01 -0700

    Agregar archivos de proyecto.

---
+++ PagosTc/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PagosTc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string codigo = DateTime.Now.ToString("yymmss");
+            Console.WriteLine(codigo);
+
+            Console.WriteLine("Introduce tu codigo de verificacion");
+            string codigoEscrito = Console.ReadLine();
+
+            if (codigoEscrito == codigo)
+            {
+                Console.WriteLine("ok");
+            }
+
+        }
+    }
+}
